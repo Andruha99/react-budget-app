33da70bfc4df8ca05dbae889a7fc02d8af0ea302
Andruha99 <Andriei.korovin.2014@mail.ru>
2023-04-25 14:11:42 +0300

    feat (p-4): add App context provider

--- src/index.tsx
+++ src/index.tsx
@@ -2,20 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { App } from "./App";
 import { GlobalStyles } from "./ui/GlobalStyles";
-import { ExpensesContextProvider } from "./context/ExensesContext/ExpensesContext";
-import { BudgetContextProvider } from "./context/BudgetContext/BudgetContext";
-import { CurrencyContextProvider } from "./context/CurrencyContext/CurrencyContext";
+import { AppContextProvider } from "./context/AppContext/AppContext";
 
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 root.render(
   <React.StrictMode>
-    <ExpensesContextProvider>
-      <BudgetContextProvider>
-        <CurrencyContextProvider>
-          <GlobalStyles />
-          <App />
-        </CurrencyContextProvider>
-      </BudgetContextProvider>
-    </ExpensesContextProvider>
+    <AppContextProvider>
+      <GlobalStyles />
+      <App />
+    </AppContextProvider>
   </React.StrictMode>,
 );
